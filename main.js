0ad28d4c52f7a984930df9c573d2fd46e98a9d7e
IArodriguez99 <73261893+IArodriguez99@users.noreply.github.com>
2021-09-22 14:55:31 -0500

    Adding CSS divs back

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 new fullpage('#fullpage', {
+  licenseKey:'6A865ECA-48BA4216-93705034-F43488F6',
   responsiveHeight: 650,
   responsiveWidth: 992,
+  autoScrolling: true,
   anchors:['issue5','issue4','issue3','issue2','issue1']
   });
